0e0ded12c7a98d0161460b0d382cb1b50a9df9c7
Dennis <github.com@mail.ds82.de>
2018-08-02 11:52:48 +0200

    chore(react-scripts): add sass-loader config to webpack

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -219,6 +219,14 @@ module.exports = {
               },
             ],
           },
+          {
+            test: /\.scss$/,
+            use: [
+              require.resolve('style-loader'), // creates style nodes from JS strings
+              require.resolve('css-loader'), // translates CSS into CommonJS
+              require.resolve('sass-loader'), // compiles Sass to CSS, using Node Sass by default
+            ],
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
@@ -229,7 +237,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
