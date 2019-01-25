b71ed188c85d4b9fab796d534cb8893dde5ff3dd
Dennis <github.com@mail.ds82.de>
2019-01-25 13:09:00 +0100

    feat(webpack): adjust webpack config

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -415,7 +415,7 @@ module.exports = function (webpackEnv) {
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
               test: /\.(js|mjs|jsx|ts|tsx)$/,
-              include: paths.appSrc,
+              exclude: /node_modules[\/\\\\](?!(@rea-jet[\/\\\\].*)[\/\\\\]).*/,
               loader: require.resolve('babel-loader'),
               options: {
                 customize: require.resolve(
@@ -426,6 +426,11 @@ module.exports = function (webpackEnv) {
                     require.resolve('babel-preset-react-app'),
                     {
                       runtime: hasJsxRuntime ? 'automatic' : 'classic',
+                    },
+                    require.resolve('@emotion/babel-preset-css-prop'),
+                    {
+                      autoLabel: true,
+                      labelFormat: '[local]',
                     },
                   ],
                 ],
