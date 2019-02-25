86592354b1391ff605d30eb9a7fa60e548cd8095
Daniel Schuech <dschuech@rea.de>
2019-02-25 11:38:57 +0100

    fix(webpack): loading of react-docgen

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -458,6 +458,7 @@ module.exports = function (webpackEnv) {
                   isEnvDevelopment &&
                     shouldUseReactRefresh &&
                     require.resolve('react-refresh/babel'),
+                  'babel-plugin-react-docgen',
                 ].filter(Boolean),
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
