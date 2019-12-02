cf4cd8d6b333c3cb5cccee8bd15a0eaa547bb0fc
Daniel Schuech <dschuech@rea.de>
2019-12-02 15:19:24 +0100

    feat(webpack): workbox should use local and rea script
    workbox should use a local copy of themself instead of using the google
    cdn. Additionally load rea-service-worker on sw start.
    
    required for preloading additional files for graphical configurator

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -614,7 +614,8 @@ module.exports = function(webpackEnv) {
         new WorkboxWebpackPlugin.GenerateSW({
           clientsClaim: true,
           exclude: [/\.map$/, /asset-manifest\.json$/],
-          importWorkboxFrom: 'cdn',
+          importWorkboxFrom: 'local',
+          importScripts: ['rea-service-worker.js'],
           navigateFallback: publicUrl + '/index.html',
           navigateFallbackBlacklist: [
             // Exclude URLs starting with /_, as they're likely an API call
@@ -653,9 +654,9 @@ module.exports = function(webpackEnv) {
           silent: true,
           formatter: typescriptFormatter,
         }),
-        new CompressionPlugin({
-          filename: '[path].gz',
-        }),
+      new CompressionPlugin({
+        filename: '[path].gz',
+      }),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
