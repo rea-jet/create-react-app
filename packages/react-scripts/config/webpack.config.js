c823c4236bc6b384345ae289bf051810f1f511ca
Daniel Schuech <dschuech@rea.de>
2025-03-13 08:28:02 +0100

    feat(react-scripts): ignore __tests__ and .spec for bundles
    dynamic imports could lead to bundling automatically whole folders
    including the specs even when they are not imported in any file.

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -711,14 +711,26 @@ module.exports = function (webpackEnv) {
           };
         },
       }),
-      // Moment.js is an extremely popular library that bundles large locale files
-      // by default due to how webpack interprets its code. This is a practical
-      // solution that requires the user to opt into importing specific locales.
-      // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
-      // You can remove this if you don't use Moment.js:
       new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/locale$/,
-        contextRegExp: /moment$/,
+        checkResource: (resource, context) => {
+          // Moment.js is an extremely popular library that bundles large locale files
+          // by default due to how webpack interprets its code. This is a practical
+          // solution that requires the user to opt into importing specific locales.
+          // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
+          // You can remove this if you don't use Moment.js:
+          if (/\.\/locale/.test(resource) && /moment/.test(context)) {
+            return true;
+          }
+
+          // rm all specs from bundles
+          if (/__tests__/.test(context)) {
+            return true;
+          }
+          if (/\.spec$/.test(resource)) {
+            return true;
+          }
+          return false;
+        }
       }),
       // Generate a service worker script that will precache, and keep up to date,
       // the HTML & assets that are part of the webpack build.
